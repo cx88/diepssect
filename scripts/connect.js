190f45a65ffb1bcc46618853e3b3cf698f9dc57d
CX8128 <cx8128@gmail.com>
2018-11-25 12:16:53 -0500

    Updated connect.js to be more of a terminal app

--- scripts/connect.js
+++ scripts/connect.js
@@ -1,46 +1,154 @@
 const BUILD = '9b9e4489cd499ded99cca19f4784fc929d21fc35'
-const SHOW_PACKETS = process.argv.includes('--show-packets')
 
-const WebSocket = this.WebSocket || require('ws')
+const WebSocket = require('ws')
+const EventEmitter = require('events')
+const readline = require('readline')
+
+const Bot = class extends EventEmitter {
+  constructor(address) {
+    super()
+
+    let ws = new WebSocket(address, null, {
+      origin: address.startsWith('wss') ? 'https://diep.io' : 'http://diep.io'
+    })
+    ws.on('open', () => {
+      super.emit('open')
+      this.send(5)
+      this.send(0, BUILD, 0, 0, 0, 0)
+
+      this.lastPing = Date.now()
+    })
+    ws.on('message', data => {
+      let u8 = new Uint8Array(data)
+      super.emit('message', u8)
+
+      switch (u8[0]) {
+        case 0:
+          super.emit('update', u8)
+          break
+        case 1:
+          super.emit('outdate', u8)
+          break
+        case 2:
+          super.emit('compressedUpdate', u8)
+          break
+        case 3:
+          super.emit('broadcast', u8)
+          break
+        case 4:
+          super.emit('status', u8)
+          break
+        case 5:
+          let now = Date.now()
+          super.emit('echo', now - this.lastPing)
+          this.send(5)
+          this.lastPing = now
+          break
+        case 6:
+          super.emit('party', u8)
+          break
+        case 7:
+          super.emit('accept')
+          break
+      }
+    })
+    ws.on('close', () => {
+      super.emit('close')
+    })
+    this.ws = ws
+  }
+  send(...args) {
+    let data = args.map(r =>
+      typeof r === 'number' ? [r] :
+      typeof r === 'string' ? r.split('').map(r => r.charCodeAt(0)) :
+    r)
+    let u8 = new Uint8Array([].concat(...data))
+    super.emit('send', u8)
+    if (this.ws.readyState === 1)
+      this.ws.send(u8)
+  }
+  close() {
+    this.ws.close()
+  }
+}
 
 // https://api.n.m28.io/endpoint/latency/findEach/
 
-let ws = new WebSocket('ws://140.82.25.222:443/', null, {
-  origin: 'http://diep.io'
-})
+let ws = null
 
-let logPacket = (dataUnknown, from) => {
-  let data = Array.from(dataUnknown)
-  if (SHOW_PACKETS)
-    console.log(`[WS] ${ from } ${ data.map(r => r.toString(16).padStart(2, '0')).join(' ') }`)
-}
-let send = data => {
-  logPacket(data, '>')
-  ws.send(new Uint8Array(data))
-}
+let rl = readline.createInterface(process.stdin, process.stdout)
+rl.setPrompt('> ')
+rl.prompt()
+rl.on('line', line => {
+  let args = line.trim().split(' ')
+  let command = args.shift() || ''
+  let options = {}
+  while (args.length && args[0].startsWith('-')) {
+    options[args.shift()] = true
+  }
+  let data = args.join(' ')
 
-ws.on('open', () => {
-  console.log('[WS] Opened')
-
-  send([5])
-  send([0, ...BUILD.split('').map(r => r.charCodeAt(0)), 0, 0, 0, 0])
-})
-ws.on('message', data => {
-  let u8 = new Uint8Array(data)
-  logPacket(u8, '<')
-
-  switch (u8[0]) {
-    case 1:
-      console.log('Client outdated!')
+  switch (command) {
+    case 'help':
+      console.log('help                display this message')
+      console.log('connect <ip>        connect to a server')
+      console.log('connect -v <ip>     connect to a server and log all packets')
+      console.log('close               close the current connection')
       break
-    case 4:
-      console.log('Server status:', Array.from(data).slice(1).map(r => r ? String.fromCharCode(r) : '; ').join(''))
+    case 'connect':
+      let verbose = options['-v']
+      if (ws) {
+        console.log('There is already an active connection')
+      } else {
+        console.log('Connecting...')
+        ws = new Bot(`ws://${ data }:443/`)
+        ws.on('open', () => log('Socket opened!'))
+        ws.on('close', () => log('Socket closed!'))
+        ws.on('outdate', () => log('Client outdated!'))
+        ws.on('status', data => log('Server status:', Array.from(data).slice(1).map(r => r ? String.fromCharCode(r) : '; ').join('')))
+        ws.on('party', data => log('Party link:', data))
+        ws.on('accept', () => log('Client accepted!'))
+        if (verbose) {
+          ws.on('send', dataUnknown => {
+            let data = Array.from(dataUnknown)
+            log(`[WS] > ${ data.map(r => r.toString(16).padStart(2, '0')).join(' ') }`)
+          })
+          ws.on('message', dataUnknown => {
+            let data = Array.from(dataUnknown)
+            log(`[WS] < ${ data.map(r => r.toString(16).padStart(2, '0')).join(' ') }`)
+          })
+        }
+      }
       break
-    case 6:
-      console.log('Party link:', data)
+    case 'close':
+      if (ws) {
+        ws.close()
+        ws = null
+        console.log('Closing...')
+      } else {
+        console.log('No active connection found')
+      }
       break
-    case 7:
-      console.log('Client accepted!')
+    case 'exit':
+      process.exit(0)
+      break
+    default:
+      console.log('`%s` is not a valid command', command)
       break
   }
+
+  rl.prompt()
+}).on('close', function() {
+  readline.clearLine(process.stdout, 0)
+  readline.cursorTo(process.stdout, 0, null)
+  console.log('> exit')
+  process.exit(0)
 })
+
+let log = (...args) => {
+  readline.clearLine(process.stdout, 0)
+  readline.cursorTo(process.stdout, 0, null)
+  console.log(...args)
+  rl.prompt(true)
+}
+
